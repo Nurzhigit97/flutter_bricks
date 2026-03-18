e535a51177bf3f56a04659baeeaad0f2d157ecec
Nurzhigit <106031957+Nurzhigit97@users.noreply.github.com>
2026-03-18 17:13:06 +0600

    Fix type declaration in {{featureName}}State.loaded factory method for improved clarity

--- feature/__brick__/lib/features/feature/presentation/cubits/{{listName}}/{{featureName}}_state.dart
+++ feature/__brick__/lib/features/feature/presentation/cubits/{{listName}}/{{featureName}}_state.dart
@@ -7,6 +7,6 @@ part '{{featureName.snakeCase()}}_state.freezed.dart';
 sealed class {{featureName.pascalCase()}}State with _${{featureName.pascalCase()}}State {
 
   const factory {{featureName.pascalCase()}}State.loading() = _{{featureName.pascalCase()}}Loading;
-  const factory {{featureName.pascalCase()}}State.loaded(List<{const {featureName.pascalCase()}}Model> data) = _{{featureName.pascalCase()}}Loaded;
+  const factory {{featureName.pascalCase()}}State.loaded(List<{{featureName.pascalCase()}}Model> data) = _{{featureName.pascalCase()}}Loaded;
   const factory {{featureName.pascalCase()}}State.error({required String error}) = _{{featureName.pascalCase()}}Error;
 }
